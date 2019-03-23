77b9df4c84b45394e937f6710100e9b82ac17fc0
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2019-03-23 09:44:37 +0200

    added simple login form

--- chess/index.js
+++ chess/index.js
@@ -1,11 +1,55 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+var cors = require('cors');
+var session = require('express-session');
+
 var app = express();
 var server = require('http').createServer(app);
 
+app.use(session({secret: 'awesome'}));
+
+app.use(bodyParser());
+app.use(cors());
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/views'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send("Hello there!");
+  if(req.session.email) {
+    res.render('index');
+  }
+  else {
+    res.render('login.ejs', {layout:false});
+  }
 });
 
-server.listen(3000, function(){
-  console.log("listening on port 3000...");
+app.post('/login',function(req, res){
+  console.log("hi");
+	req.session.email = req.body.email;
+	req.session.password = req.body.pass;
+	res.end('done');
 });
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/logout',function(req,res){
+
+	req.session.destroy(function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect('/');
+		}
+	});
+
+});
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running on port 3000...');
