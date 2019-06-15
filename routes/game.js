a3cf39fdf3116c933b9f49488d233c6ea58969fb
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2019-06-15 21:09:29 +0300

    when the user tries to go to game that does not exsit the server returns 404

--- routes/game.js
+++ routes/game.js
@@ -14,10 +14,25 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 router.get('/:white/:black', function(req, res){
-  res.render('about',{ layout: false,
-    user: req.session.username,
-    white: req.params.white,
-    black: req.params.black
+  var player1 = req.params.white;
+  var player2 = req.params.black;
+  GameModel.findOne({player1: player1, player2: player2, roomno: req.query.roomno}, function(err, result){
+      if (err){
+        throw err;
+      }else {
+        if(result == null){
+          res.status(404);
+          res.end("No such game");
+        }else {
+          res.render('about',{ layout: false,
+            user: req.session.username,
+            white: player1,
+            black: player2
+          });
+          res.status(200);
+          res.end();
+        }
+      }
   });
 });
 
